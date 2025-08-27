121249ebe92dec3890af49fc0822be53a6486ac6
Lu Sostisso <108033299+lusostisso@users.noreply.github.com>
2025-08-27 18:40:05 +0000

    hello funcionando

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+HOST=`hostname -I | awk '{print $1}'`
+cat $BASE_DIR/../custom-scripts/network-config | sed 's/\[IP-DO-HOST\]/'"$HOST"'/g' > $BASE_DIR/../custom-scripts/S41network-config
+cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
+chmod +x $BASE_DIR/target/etc/init.d/S41network-config
 
-if [ -d "$BASE_DIR" ]; then
-    cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
-    chmod +x $BASE_DIR/target/etc/init.d/S41network-config
-fi
+cp $BASE_DIR/../custom-scripts/S50hello $BASE_DIR/target/etc/init.d
+chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+$HOST_CC $$HOST_CC ${BASE_DIR}/apps/hello.c -O2 -o ${BASE_DIR}/apps/hello
+cp ${BASE_DIR}/apps/hello ${TARGET_DIR}/usr/bin/
+cp ${BASE_DIR}/custom-scripts/S50hello ${TARGET_DIR}/etc/init.d/
+chmod +x ${TARGET_DIR}/usr/bin/hello
+chmod +x ${TARGET_DIR}/etc/init.d/S50hello
+
+cp board/labsisop/linuxstatus.py output/target/usr/bin/
+chmod +x output/target/usr/bin/linuxstatus.py
+chmod +x output/target/etc/init.d/S50linuxstatus
